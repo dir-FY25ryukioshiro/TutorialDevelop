7965be733523be9f5c18415dd940d0e8ad89a6e2
dir-FY25ryukioshiro <ryuki.oshiro.a766@dirbato.co.jp>
2025-06-12 12:17:14 +0900

    lesson21

--- TutorialDevelop/src/main/java/com/dirbato/service/UserService.java
+++ TutorialDevelop/src/main/java/com/dirbato/service/UserService.java
@@ -1,37 +1,29 @@
 package com.dirbato.service;
+
 import java.util.List;
-import java.util.Set; // 追加
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional; // 追加
 import com.dirbato.entity.User;
 import com.dirbato.repository.UserRepository;
+
 @Service
 public class UserService {
-    private final UserRepository userRepository;
-    public UserService(UserRepository repository) {
-        this.userRepository = repository;
-    }
+
+    @Autowired
+    private UserRepository userRepository;
+
     /** 全件を検索して返す */
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
-    /** Userを1件検索して返す */
-    public User getUser(Integer id) {
-        return userRepository.findById(id).get();
-    }
-    /** Userの登録を行なう */
+
+    // ----- 追加:ここから ----
+    /** Userの登録を行う */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
-    }
-    // ----- 追加:ここから ----
-    /** Userの削除を行なう */
-    @Transactional
-    public void deleteUser(Set<Integer> idck) {
-        for(Integer id : idck) {
-            userRepository.deleteById(id);
-        }
     }
     // ----- 追加:ここまで ----
 }
